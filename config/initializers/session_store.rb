7a87cdaa98f644ef1344f2fb51f94d1db2a7405d
Thomas Baird <tbaird@mail.umw.edu>
2016-09-19 01:37:54 -0400

    Removing subdomain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_tmobaird_session', domain: {
-  production: '.tmobaird.io',
-  development: 'lvh.me'
-}.fetch(Rails.env.to_sym, :all)
+Rails.application.config.session_store :cookie_store, key: '_tmobaird_session'
--- spec/routing/posts_routing_spec.rb
+++ spec/routing/posts_routing_spec.rb
@@ -1,14 +1,14 @@
 require "rails_helper"
 
 RSpec.describe PostsController, type: :routing do
-  xdescribe "routing" do
+  describe "routing" do
 
     it "routes to #index" do
-      expect(:get => posts_path, subdomain: 'blog').to route_to("posts#index")
+      expect(:get => "/posts").to route_to("posts#index")
     end
 
     it "routes to #new" do
-      expect(:get => "http://blog.lvh.me/posts/new").to route_to("posts#new")
+      expect(:get => "/posts/new").to route_to("posts#new")
     end
 
     it "routes to #show" do
