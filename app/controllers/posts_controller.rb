a2ec4f9b6de278bce2edfffff9858e6a7df98bbd
Thomas Baird <tbaird@mail.umw.edu>
2017-01-19 01:24:38 -0500

    Fixing the disqus url. Again

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,7 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
-    @post_url = "http://tmobaird.io/posts/#{@post.slug}"
+    @post_url = "http://www.tmobaird.io/posts/#{@post.slug}"
   end
 
   # GET /posts/new
